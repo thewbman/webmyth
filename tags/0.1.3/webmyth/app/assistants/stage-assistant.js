56e7ce6c1ce6ce36b2b54b1bb7e189c60497dde3
thewbman <thewbman@4db19354-8cae-8bec-cf2f-4c87da6f5ba3>
2010-08-30 08:12:52 +0000

    Tag 0.1.3

---
+++ tags/0.1.3/webmyth/app/assistants/stage-assistant.js
@@ -0,0 +1,169 @@
+/*
+ *   WebMyth - An open source webOS app for controlling a MythTV frontend. 
+ *   http://code.google.com/p/WebMyth/
+ *   Copyright (C) 2010  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+ 
+ function StageAssistant() {
+	/* this is the creator function for your stage assistant object */
+}
+
+WebMyth = {};
+
+ 
+/********************Globals**************************/
+//Setup App Menu
+WebMyth.appMenuAttr = {omitDefaultItems: true};
+WebMyth.appMenuModel = {
+	visible: true,
+	items: [
+		{label: "About...", command: 'do-aboutApp'},
+		{label: "Preferences", command: 'do-prefsApp'}
+	]
+};
+//Create WebMyth.db for use or open existing
+WebMyth.db;
+	
+	
+//Setup remote commandmenu
+WebMyth.remoteCommandMenuModel = {
+	visible: true,
+	items: [{},{
+		items: [
+			{label: "Nav", command: 'go-navigation', width: 90},
+			{label: "Play", command: 'go-playback', width: 70},
+			{label: "Music", command: 'go-music', width: 90}
+		]
+		},
+	{}
+	]
+};
+	
+//Setup header menu button
+WebMyth.headerMenuButtonModel = {
+	 label: "...",
+	 buttonClass:'small-button',
+     disabled: false, 
+	 command: 'go-headerMenu'
+};
+	
+//Current frontend host
+WebMyth.activeHost = 'Undefined';
+WebMyth.activePort = '6546';
+	
+//Cookie for preferences
+WebMyth.prefsCookie = new Mojo.Model.Cookie('prefs');
+WebMyth.prefsCookieObject = WebMyth.prefsCookie.get();
+
+
+
+
+
+StageAssistant.prototype.setup = function() {
+	/* this function is for setup tasks that have to happen when the stage is first created */
+	
+	//Instantiate Metrix Library
+	WebMyth.Metrix = new Metrix(); 
+	
+	//Setup db
+	WebMyth.db = createHostnameDb();
+	
+	//Handle message command from plug-in
+	//$('telnetPlug').pluginMessageFunc = this.pluginMessageFunc.bind(this); 
+	
+	
+	//Start first scene
+	this.controller.pushScene("welcome");
+	
+};
+
+StageAssistant.prototype.handleCommand = function(event) {
+  var currentScene = this.controller.activeScene();
+  if(event.type == Mojo.Event.command) {
+    switch(event.command) {
+      case 'do-aboutApp':
+        
+			aboutinfo = "<a href='http://code.google.com/p/WebMyth/'>WebMyth Homepage</a><hr/>";
+			
+			aboutinfo += "An open source webOS app for controlling a MythTV frontend.<br>";  
+			aboutinfo += "Please see the homepage for system requirements.<hr/>"
+			
+			aboutinfo += "Licensed under <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>."
+
+            currentScene.showAlertDialog({
+                onChoose: function(value) {},
+                title: "WebMyth - v" + Mojo.Controller.appInfo.version,
+                message: "Copyright 2010, Wes Brown <br>" + aboutinfo,
+                choices: [{
+                    label: "OK",
+                    value: ""
+                }],
+                allowHTMLMessage: true
+            });
+
+       break;
+	   
+	  case 'do-prefsApp':
+			this.controller.pushScene("preferences");
+       break;
+	   
+	  case 'do-recorded':
+			this.controller.pushScene("recorded");
+       break;
+	   
+	  case 'go-navigation':
+			if(currentScene == 'navigation'){
+				Mojo.Log.info("Already on navigation");
+			} else {
+				Mojo.Controller.stageController.swapScene("navigation");
+			}
+	   break;
+	   
+	  case 'go-playback':
+			if(currentScene == 'playback'){
+				Mojo.Log.info("Already on playback");
+			} else {
+				Mojo.Controller.stageController.swapScene("playback");
+			}
+	   break;
+	   
+	  case 'go-music':
+			if(currentScene == 'music'){
+				Mojo.Log.info("Already on music");
+			} else {
+				Mojo.Controller.stageController.swapScene("music");
+			}
+	   break;
+    }
+  }
+};
+
+pluginMessageFunc = function(a)       
+{       
+      //Send up an alert dialog with message from plug-in
+	  currentScene.showAlertDialog({
+                onChoose: function(value) {},
+                title: "Message from plug-in:",
+                message: String(a),
+                choices: [{
+                    label: "OK",
+                    value: ""
+                }],
+                allowHTMLMessage: true
+            });   
+}; 
---
+++ tags/0.1.3/webmyth/app/models/webmythCommands.js
@@ -0,0 +1,123 @@
+/*
+ *   WebMyth - An open source webOS app for controlling a MythTV frontend. 
+ *   http://code.google.com/p/WebMyth/
+ *   Copyright (C) 2010  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+var telnetClass = Class.create({
+	initialize: function() {
+	}
+});
+
+
+function createHostnameDb() {
+	
+	//Create WebMyth.db or open it is exists.
+	
+	var newdb = openDatabase('hostnames', "0.1", "Hostname db");
+	
+	//Host (frontends) table
+	newdb.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS hosts(id INTEGER PRIMARY KEY, hostname TEXT, port INTEGER, ipChar TEXT)', 
+      []);
+    });
+	
+	
+	//Recorded table
+	newdb.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS recorded(id INTEGER PRIMARY KEY, chanid INTEGER, starttime TEXT, endtime TEXT, title TEXT, subtitle TEXT, description TEXT, category TEXT, hostname TEXT, bookmark INTEGER, editing INTEGER, cutlist INTEGER, autoexpire INTEGER, commflagged INTEGER, recgroup TEXT, recordid INTEGER, seriesid TEXT, programid TEXT, lastmodified TEXT, filesize INTEGER, stars REAL, previouslyshown INTEGER, originalairdate TEXT, preserve INTEGER, findid INTEGER, deletepending INTEGER, transcoder INTEGER, timestretch REAL, recpriority INTEGER, basename TEXT, progstart TEXT, progend TEXT, playgroup TEXT, profile TEXT, duplicate INTEGER, transcoded INTEGER, watched INTEGER, storagegroup TEXT, bookmarkupdate TEXT)', 
+      []);
+    });
+	
+	return newdb;
+	
+};
+
+function is_int(value){
+	  if((parseFloat(value) == parseInt(value)) && !isNaN(parseInt(value))){
+	      return true;
+	 } else {
+	      return false;
+	 }
+};
+
+function openTelnet(telnetPlug, hostname, port) {
+	//Start telnet communication
+	Mojo.Controller.errorDialog("Connecting to %s", $('telnetPlug'));
+	telnetPlug.OpenTelnetConnection(hostname, port);
+	Mojo.Log.info("Opened telnet connection to %s", hostname);
+};
+
+function sendKey(telnetPlug, value) {
+	//Send key command to telnet host
+	sendCommand(telnetPlug, "key "+value);
+};
+
+function sendCommand(telnetPlug, value) {
+	//Send command to telnet host
+	$('telnetPlug').SendTelnet(value);
+	
+	Mojo.Log.info("Sending command '%s' to host", value);
+};
+
+function sendCommandwithReply(telnetPlug, value) {
+	//Not currently working ...
+	var response = telnetPlug.SendTelnetWithReply(value);
+	//var reponse = "response";
+	
+	return response;
+};
+
+function closeTelnet(telnetPlug) {
+	//Close out telnet connection
+	telnetPlug.CloseTelnetConnection();
+	
+	Mojo.Log.info("Closing telnet connection");
+};
+
+function defaultCookie() {
+	var newCookieObject = {
+		webserverName: '',
+		webserverRemoteFile: '/cgi-bin/remote.py',
+		webMysqlFile: '/webmyth-mysql.php',
+		allowMetrix: true
+	};
+	
+	return newCookieObject;
+};
+
+
+var sort_by = function(field, reverse, primer){
+
+   reverse = (reverse) ? -1 : 1;
+
+   return function(a,b){
+
+       a = a[field];
+       b = b[field];
+
+       if (typeof(primer) != 'undefined'){
+           a = primer(a);
+           b = primer(b);
+       }
+
+       if (a<b) return reverse * -1;
+       if (a>b) return reverse * 1;
+       return 0;
+
+   }
+};
