0ad8fafdd484479c06d5033fb1fda1290eb1da0c
thewbman <thewbman@4db19354-8cae-8bec-cf2f-4c87da6f5ba3>
2010-09-15 06:29:34 +0000

    Tag 0.2.1

---
+++ tags/0.2.1/webmyth/app/assistants/preferences-assistant.js
@@ -0,0 +1,216 @@
+/*
+ *   WebMyth - An open source webOS app for controlling a MythTV frontend. 
+ *   http://code.google.com/p/webmyth/
+ *   Copyright (C) 2010  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+ function PreferencesAssistant() {
+	/* this is the creator function for your scene assistant object. It will be passed all the 
+	   additional parameters (after the scene name) that were passed to pushScene. The reference
+	   to the scene controller (this.controller) has not be established yet, so any initialization
+	   that needs the scene controller should be done in the setup function below. */
+}
+
+PreferencesAssistant.prototype.setup = function() {
+	/* this function is for setup tasks that have to happen when the scene is first created */
+		
+	//App menu widget
+	this.controller.setupWidget(Mojo.Menu.appMenu, WebMyth.appMenuAttr, WebMyth.appMenuModel);	
+		
+	//Widgets
+	this.webserverTextModel = {
+             value: "",
+             disabled: false
+    };
+	this.controller.setupWidget("webserverTextFieldId",
+        {
+            hintText: $L(""),
+            multiline: false,
+            enterSubmits: false,
+            focus: false
+         },
+         this.webserverTextModel
+    ); 
+	/*
+	this.webserverRemoteFileTextModel = {
+             value: "/cgi-bin/remote.py",
+             disabled: false
+    };
+	this.controller.setupWidget("webserverRemoteFileFieldId",
+        {
+            hintText: $L("/cgi-bin/remote.py"),
+            multiline: false,
+            enterSubmits: false,
+            focus: false
+         },
+         this.webserverRemoteFileTextModel
+    ); 
+	
+	this.webMysqlFileTextModel = {
+             value: "/webmyth-mysql.php",
+             disabled: false
+    };
+	this.controller.setupWidget("webMysqlFileFieldId",
+        {
+            hintText: $L("/webmyth-mysql.php"),
+            multiline: false,
+            enterSubmits: false,
+            focus: false
+         },
+         this.webMysqlFileTextModel
+    ); 
+	*/
+	this.webmythPythonFileTextModel = {
+             value: "/cgi-bin/webmyth.py",
+             disabled: false
+    };
+	this.controller.setupWidget("webmythPythonFieldId",
+        {
+            hintText: $L("/cgi-bin/webmyth.py"),
+            multiline: false,
+            enterSubmits: false,
+            focus: false
+         },
+         this.webmythPythonFileTextModel
+    ); 
+	
+	this.themeModel = {
+			value: WebMyth.prefsCookieObject.theme,
+            disabled: false
+	};
+	this.controller.setupWidget('theme',
+		{
+			label: $L("Theme"),
+			choices:[
+				{label:$L("Palm Default"),      value:'palm-default'},
+				{label:$L("Palm Dark"),         value:'palm-dark'}
+			]//,
+			//modelProperty: 'theme'
+		},
+		this.themeModel
+	);
+	this.controller.listen('theme', Mojo.Event.propertyChange, this.themeChanged.bindAsEventListener(this));
+	
+	
+	this.metrixToggleModel = {
+             value: true
+    };
+	this.controller.setupWidget("metrixToggleId",
+        {
+            modelProperty: "value"
+         },
+         this.metrixToggleModel
+    ); 
+	
+	this.controller.setupWidget("saveWebserverButtonId",
+         {},
+         {
+             label : "SAVE",
+             disabled: false
+         }
+     );
+	
+	/* add event handlers to listen to events from widgets */
+	Mojo.Event.listen(this.controller.get("saveWebserverButtonId"),Mojo.Event.tap, this.saveWebserver.bind(this));
+	
+	/* add event handlers to listen to events from widgets */
+};
+
+PreferencesAssistant.prototype.activate = function(event) {
+
+		if (WebMyth.prefsCookieObject) {	
+			Mojo.Log.info("Existing webserverName is %s", WebMyth.prefsCookieObject.webserverName);
+			
+			//Update webserver address from cookie
+			this.webserverTextModel.value = WebMyth.prefsCookieObject.webserverName;
+			this.controller.modelChanged(this.webserverTextModel);
+			
+			
+			//Update filenames on web server if set
+			/*
+			if ( WebMyth.prefsCookieObject.webserverRemoteFile == null ) {
+				Mojo.Log.error("Did not find remote file in cookie");
+			} else {
+				Mojo.Log.info("Found remote file in cookie '%s'", WebMyth.prefsCookieObject.webserverRemoteFile);
+				this.webserverRemoteFileTextModel.value = WebMyth.prefsCookieObject.webserverRemoteFile;
+				this.controller.modelChanged(this.webserverRemoteFileTextModel);
+			}
+			if ( WebMyth.prefsCookieObject.webMysqlFile == null ) {
+				Mojo.Log.error("Did not find mysql file in cookie");
+			} else {
+				Mojo.Log.info("Found mysql file in cookie '%s'", WebMyth.prefsCookieObject.webMysqlFile);
+				this.webMysqlFileTextModel.value = WebMyth.prefsCookieObject.webMysqlFile;
+				this.controller.modelChanged(this.webMysqlFileTextModel);
+			}
+			*/
+			if ( WebMyth.prefsCookieObject.webmythPythonFile == null ) {
+				Mojo.Log.error("Did not find python file in cookie");
+			} else {
+				Mojo.Log.info("Found puthon file in cookie '%s'", WebMyth.prefsCookieObject.webmythPythonFile);
+				this.webmythPythonFileTextModel.value = WebMyth.prefsCookieObject.webmythPythonFile;
+				this.controller.modelChanged(this.webmythPythonFileTextModel);
+			}
+			
+			//Update metrix toggle from cookie
+			this.metrixToggleModel.value = WebMyth.prefsCookieObject.allowMetrix;
+			this.controller.modelChanged(this.metrixToggleModel);
+			
+		} 
+};
+
+PreferencesAssistant.prototype.deactivate = function(event) {
+	/* remove any event handlers you added in activate and do any other cleanup that should happen before
+	   this scene is popped or another scene is pushed on top */
+};
+
+PreferencesAssistant.prototype.cleanup = function(event) {
+	/* this function should do any cleanup needed before the scene is destroyed as 
+	   a result of being popped off the scene stack */
+};
+
+PreferencesAssistant.prototype.themeChanged = function(event) {
+	this.controller.document.body.className = event.value;
+};
+
+PreferencesAssistant.prototype.saveWebserver = function(event) {
+	
+	Mojo.Log.info("New webserverName is %s", this.webserverTextModel.value);
+	//Mojo.Log.info("New remote file is %s", this.webserverRemoteFileTextModel.value);
+	//Mojo.Log.info("New mysql file is %s", this.webMysqlFileTextModel.value);
+	Mojo.Log.info("New python file is %s", this.webmythPythonFileTextModel.value);
+	Mojo.Log.info("Metrix value is %s", this.metrixToggleModel.value);
+	Mojo.Log.info("Theme value is %s", this.themeModel.value);
+
+	if (WebMyth.prefsCookieObject) {
+		//Nothing
+	} else {
+		//Create default cookie if doesnt exist
+		var newPrefsCookieObject = defaultCookie();
+		WebMyth.prefsCookieObject = newPrefsCookieObject;
+	}
+	
+	WebMyth.prefsCookieObject.webserverName = this.webserverTextModel.value;
+	//WebMyth.prefsCookieObject.webserverRemoteFile = this.webserverRemoteFileTextModel.value;
+	//WebMyth.prefsCookieObject.webMysqlFile = this.webMysqlFileTextModel.value;
+	WebMyth.prefsCookieObject.webmythPythonFile = this.webmythPythonFileTextModel.value;
+	WebMyth.prefsCookieObject.allowMetrix = this.metrixToggleModel.value;
+	WebMyth.prefsCookieObject.theme = this.themeModel.value;
+	WebMyth.prefsCookie.put(WebMyth.prefsCookieObject);
+	
+
+	Mojo.Controller.stageController.popScene();
+};
---
+++ tags/0.2.1/webmyth/app/assistants/stage-assistant.js
@@ -0,0 +1,238 @@
+/*
+ *   WebMyth - An open source webOS app for controlling a MythTV frontend. 
+ *   http://code.google.com/p/WebMyth/
+ *   Copyright (C) 2010  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+ 
+ function StageAssistant() {
+	/* this is the creator function for your stage assistant object */
+}
+
+WebMyth = {};
+
+ 
+/********************Globals**************************/
+//Setup App Menu
+WebMyth.appMenuAttr = {omitDefaultItems: true};
+WebMyth.appMenuModel = {
+	visible: true,
+	items: [
+		{label: "About...", command: 'do-aboutApp'},
+		{label: "Preferences", command: 'do-prefsApp'},
+		{label: "Help", command: 'do-helpApp'}
+	]
+};
+//Create WebMyth.db for use or open existing
+WebMyth.db;
+	
+	
+//Setup remote commandmenu
+WebMyth.remoteCommandMenuAttr = { menuClass: 'no-fade' };	
+WebMyth.remoteCommandMenuModel = {
+	visible: true,
+	items: [{},{
+		items: [
+			{label: "Nav", command: 'go-navigation', width: 90},
+			{label: "Play", command: 'go-playback', width: 70},
+			{label: "Music", command: 'go-music', width: 90}
+		]
+		},
+	{}
+	]
+};
+
+WebMyth.remoteCommandMenuModel = {
+	visible: true,
+	items: [{},{
+		items: [
+			{label: "Nav", command: 'go-navigation', width: 90},
+			{label: "Play", command: 'go-playback', width: 70},
+			{label: "Music", command: 'go-music', width: 90}
+		]
+		},
+	{}
+	]
+};
+	
+//Setup header menu button
+WebMyth.headerMenuButtonModel = {
+	 label: "...",
+	 buttonClass:'small-button',
+     disabled: false, 
+	 command: 'go-headerMenu'
+};
+	
+	
+//Cookie for preferences
+WebMyth.prefsCookie = new Mojo.Model.Cookie('prefs');
+WebMyth.prefsCookieObject = WebMyth.prefsCookie.get();
+	
+	
+//Cookie for hosts
+WebMyth.hostsCookie = new Mojo.Model.Cookie('hosts');
+WebMyth.hostsCookieObject = WebMyth.hostsCookie.get();
+
+
+//Current script verion
+//2 = 0.1.8, 3 = 0.1.9, 4 = 0.2.0
+WebMyth.currentScriptVersion = 4;
+
+
+//Help and first-run message
+WebMyth.helpMessage = "This app requires the installation of 1 script on a local webserver on your network.  ";
+WebMyth.helpMessage += "You can get the file <a href='http://code.google.com/p/webmyth/downloads/list'>here</a><hr/>";
+WebMyth.helpMessage += "If you have installed previous verions of the script files please upgrade them to the latest versions.";
+//WebMyth.helpMessage += "The remote script (<a href='http://code.google.com/p/webmyth/source/browse/trunk/remote.py'>remote.py</a>) ";
+//WebMyth.helpMessage += "needs to be in executable as cgi-bin while the mysql script (<a href='http://code.google.com/p/webmyth/source/browse/trunk/webmyth-mysql.php'>webmyth-mysql.php</a>) can be in any standard directory.  ";
+//WebMyth.helpMessage += "Both files needs to be accesible to this device without any authentication.<hr/>";
+
+
+WebMyth.helpEmailText = "This app requires the installation of 1 script on a local webserver on your network.  ";
+WebMyth.helpEmailText += "You can get the file <a href='http://code.google.com/p/webmyth/downloads/list'>here</a><hr/>";
+WebMyth.helpEmailText += "The script (<a href='http://code.google.com/p/webmyth/source/browse/trunk/webmyth.py'>webmyth.py</a>) ";
+WebMyth.helpEmailText += "needs to be in executable as cgi-bin on your local webserver.  ";
+WebMyth.helpEmailText += "The script has some database settings that you need to manually set.  You can find the correct values for your system by looking at /etc/mythtv/mysql.txt on your frontend or backend machine.<hr/>";
+WebMyth.helpEmailText += "The file needs to be accesible to this device without any authentication.<hr/>";
+WebMyth.helpEmailText += "Please report any issues you find with the system on the 'issues' tab of the homepage.  ";
+WebMyth.helpEmailText += "Or you can email the developer directly at <a href=mailto:thewbman+webmyth@gmail.com>thewbman@gmail.com</a>.";
+
+
+StageAssistant.prototype.setup = function() {
+	/* this function is for setup tasks that have to happen when the stage is first created */
+	
+	//Instantiate Metrix Library
+	WebMyth.Metrix = new Metrix(); 
+	
+	//Setup db
+	WebMyth.db = createHostnameDb();
+	
+	//Handle message command from plug-in
+	//$('telnetPlug').pluginMessageFunc = this.pluginMessageFunc.bind(this); 
+	
+	
+	//Start first scene
+	this.controller.pushScene("welcome");
+	
+};
+
+StageAssistant.prototype.handleCommand = function(event) {
+  var currentScene = this.controller.activeScene();
+  if(event.type == Mojo.Event.command) {
+    switch(event.command) {
+      case 'do-aboutApp':
+        
+			aboutinfo = "<a href='http://code.google.com/p/webmyth/'>WebMyth Homepage</a><hr/>";
+			
+			aboutinfo += "An open source webOS app for controlling a MythTV frontend.<br>";  
+			aboutinfo += "Please see the homepage for system requirements and setup instructions.<hr/>"
+			
+			aboutinfo += "Licensed under <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>."
+
+            currentScene.showAlertDialog({
+                onChoose: function(value) {if (value==true) {
+					Mojo.Log.error("appPath:" + Mojo.appPath);
+					this.controller.serviceRequest(
+						"palm://com.palm.applicationManager", {
+							method: 'open',
+							parameters: {
+								id: "com.palm.app.email",
+								params: {
+									summary: "WebMyth setup instructions",
+									text: WebMyth.helpEmailText
+								}
+							}
+						}
+					);
+					}
+				},
+				title: "WebMyth - v" + Mojo.Controller.appInfo.version,
+                message: "Copyright 2010, Wes Brown <br>" + aboutinfo,
+                choices: [
+					{label: "OK", value: false},
+					{label: "Email Instructions", value: true}
+					],
+                allowHTMLMessage: true
+            });
+
+       break;
+	   
+	  case 'do-prefsApp':
+			this.controller.pushScene("preferences");
+       break;
+	   
+	  case 'do-helpApp':
+			//helpAlert();
+	
+			currentScene.showAlertDialog({
+				onChoose: function(value) {if (value==true) {
+					//Mojo.Log.error("appPath:" + Mojo.appPath);
+					this.controller.serviceRequest(
+						"palm://com.palm.applicationManager", {
+							method: 'open',
+							parameters: {
+								id: "com.palm.app.email",
+								params: {
+									summary: "WebMyth setup instructions",
+									text: WebMyth.helpEmailText
+								}
+							}
+						}
+					);
+					}
+				},
+				title: "WebMyth - v" + Mojo.Controller.appInfo.version,
+				message:  WebMyth.helpMessage, 
+				choices: [
+                    {label: "OK", value: false},
+					{label: "Email this", value: true}
+					],
+				allowHTMLMessage: true
+			});	
+			
+       break;
+	   
+	  case 'do-recorded':
+			this.controller.pushScene("recorded");
+       break;
+	   
+	  case 'go-navigation':
+			if(currentScene == 'navigation'){
+				Mojo.Log.info("Already on navigation");
+			} else {
+				Mojo.Controller.stageController.swapScene("navigation");
+			}
+	   break;
+	   
+	  case 'go-playback':
+			if(currentScene == 'playback'){
+				Mojo.Log.info("Already on playback");
+			} else {
+				Mojo.Controller.stageController.swapScene("playback");
+			}
+	   break;
+	   
+	  case 'go-music':
+			if(currentScene == 'music'){
+				Mojo.Log.info("Already on music");
+			} else {
+				Mojo.Controller.stageController.swapScene("music");
+			}
+	   break;
+    }
+  }
+};
